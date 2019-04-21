5a9cb7ddb30c1f1cb4fed1c1b03e2a14af468c0a
yuanqing <desk@yuanqing.sg>
2019-04-21 16:39:43 +0800

    Expose `sortByName`, make sorting deterministic by comparing ids

--- src/attributes/name.js
+++ src/attributes/name.js
@@ -3,10 +3,17 @@ import naturalCompare from 'natural-compare-lite'
 import sortLayersFactory from '../sort-layers-factory'
 
 export default sortLayersFactory({
-  sortLayers: function (layers) {
-    return layers.sort(function (a, b) {
-      return naturalCompare(a.name.toLowerCase(), b.name.toLowerCase())
-    })
-  },
+  sortLayers: sortByName,
   successMessage: 'Sorted layers by name'
 })
+
+export function sortByName (layers) {
+  return layers.sort(function (a, b) {
+    const aName = a.name.toLowerCase()
+    const bName = b.name.toLowerCase()
+    if (aName === bName) {
+      return naturalCompare(a.id, b.id)
+    }
+    return naturalCompare(aName, bName)
+  })
+}
